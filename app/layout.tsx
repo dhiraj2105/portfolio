90341fd18da83607389fe67a71afa483c1ccfbb4
dhiraj2105 <dkskp2005@gmail.com>
2026-05-31 15:15:19 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import { Inter, IBM_Plex_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 
@@ -63,6 +64,7 @@ export default function RootLayout({
         `}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
